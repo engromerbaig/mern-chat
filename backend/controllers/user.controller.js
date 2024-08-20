a2f115e4de9cb62402234de4a4e8c955dd51fbf9
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-08-20 08:34:50 +0200

    only approved users showing in the sidebar now!

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,18 @@
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
-	try {
-		const loggedInUserId = req.user._id;
+  try {
+    const loggedInUserId = req.user._id;
 
-		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    // Fetch users who are not the logged-in user and whose role request status is approved
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+      roleRequestStatus: 'approved'  // Only include users with approved role requests
+    }).select("-password");
 
-		res.status(200).json(filteredUsers);
-	} catch (error) {
-		console.error("Error in getUsersForSidebar: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
