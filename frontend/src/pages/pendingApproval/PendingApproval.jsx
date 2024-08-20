5eb671e5717a889490a26d229a6529952a38755f
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-08-20 06:10:21 +0200

    created a pending  page on frontend for users after they signup

--- frontend/src/pages/pendingApproval/PendingApproval.jsx
+++ frontend/src/pages/pendingApproval/PendingApproval.jsx
@@ -1,21 +1,15 @@
 // src/pages/PendingApproval.jsx
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';  // Optional: For setting the page title
 
 const PendingApproval = () => {
   return (
-    <div className="pending-approval">
-      <Helmet>
-        <title>Pending Approval</title>
-      </Helmet>
-      <h1>Pending Approval</h1>
-      <p>Your request has been submitted successfully.</p>
-      <p>
-        You will be notified once your role request is approved. In the meantime, you can <Link to="/">return to the home page</Link> or check your email for updates.
-      </p>
-      <p>If you have any questions, please contact support.</p>
+    <div>
+      <h1 className='text-3xl text-white text-center'>Your request has been submitted successfully.</h1>
+      <p className='text-xl text-center text-gray-300'>
+        You will be notified once your role request is approved. </p>
+        <p className='text-xl text-center text-gray-300'>
+        Thank you for your patience.</p>
     </div>
   );
 };
