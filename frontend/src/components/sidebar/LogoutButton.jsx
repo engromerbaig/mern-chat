675fc93ea9e7da0aabb2c09c4beb5b6fb4f96be5
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-08-26 04:50:31 +0200

    added return to dashboard button for admin

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,17 +1,39 @@
 import { BiLogOut } from "react-icons/bi";
+import { useNavigate, useLocation } from 'react-router-dom';
 import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-	const { loading, logout } = useLogout();
+  const { loading, logout, role } = useLogout();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	return (
-		<div className='mt-auto'>
-			{!loading ? (
-				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
-			) : (
-				<span className='loading loading-spinner'></span>
-			)}
-		</div>
-	);
+  const handleDashboardRedirect = () => {
+    navigate('/admin-dashboard'); // Adjust the path as necessary
+  };
+
+  // Determine if we are on the /admin-dashboard page
+  const isOnDashboard = location.pathname === '/admin-dashboard';
+
+  return (
+    <div className='mt-auto flex items-center'>
+      {!loading ? (
+        <>
+          {/* Show the Return to Dashboard button only if the user is a Super Admin and not on the dashboard page */}
+          {role === 'Super Admin' && !isOnDashboard && (
+            <button
+              onClick={handleDashboardRedirect}
+              className='text-white text-sm bg-blue-500 px-2 py-0 rounded-lg hover:bg-blue-600 transition duration-300 mr-4'
+            >
+              Return to Dashboard
+            </button>
+          )}
+          <BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+        </>
+      ) : (
+        <span className='loading loading-spinner'></span>
+      )}
+    </div>
+  );
 };
+
 export default LogoutButton;
