da79e8d3ecf46e375618c425937bde7a45cb6d15
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-10-16 22:04:00 +0500

    updated theme color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  sky: {
+			500: '#1E3A5F',  // Update the sky-500 color to your custom hex value
+		  },
+		},
+	  },
 	},
 	// eslint-disable-next-line no-undef
 	plugins: [require("daisyui")],
-};
+  };
+  
